c68f2ac079b0da033f30df74842ae80e8fd1c0b5
Chris Malone <malone.chrisj@gmail.com>
2025-04-11 13:05:09 +1000

    Move test file and update cases

---
+++ test_mckp.py
@@ -0,0 +1,53 @@
+import pyarrow as pa
+import mckp
+
+# Create a test ListArray: [[1, 2], [3], [4, 5, 6]]
+# offer_ids = pa.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]], type=pa.list_(pa.uint32()))
+# rewards = pa.array([[0, 110, 200], [0, 230, 300], [0, 210, 500]], type=pa.list_(pa.float64()))
+# costs = pa.array([[0, 100, 200], [0, 200, 300], [0, 200, 400]], type=pa.list_(pa.float64()))
+# budget_constraint = 500
+
+
+# Create test data with more customers and smaller numbers
+offer_ids = pa.array([
+    [0, 1, 2], 
+    [0, 1, 2], 
+    [0, 1, 2], 
+    [0, 1, 2], 
+    [0, 1, 2]
+], type=pa.list_(pa.uint32()))
+
+rewards = pa.array([
+    [0, 15, 22], 
+    [0, 18, 32], 
+    [0, 10, 19], 
+    [0, 17, 28], 
+    [0, 8, 18]
+], type=pa.list_(pa.float64()))
+
+costs = pa.array([
+    [0, 10, 20], 
+    [0, 15, 25], 
+    [0, 8, 16], 
+    [0, 12, 22], 
+    [0, 7, 14]
+], type=pa.list_(pa.float64()))
+
+budget_constraint = 50
+
+print("Python ListArray:", offer_ids)
+print("\nC++ Analysis:")
+
+results = mckp.mckp.solve(
+    offer_ids,
+    rewards,
+    costs,
+    budget_constraint,
+    0,
+)
+
+print(dir(results))
+print(results)
+
+print(f"{results['spend'][-2]=}")
+print(f"{results['gain'][-2]=}")
